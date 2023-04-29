e708fc1979c70856135d748af08dd17a5a422c9a
Caio Ramos <ARamosCaio@.gmail.com>
2023-04-28 21:39:07 -0300

    feat: added message processing script

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-from flask import Flask, requests, jsonify
+from flask import Flask, request, jsonify
 from ultrabot import ultraChatBot
 import json
 
 app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def home():
+    if request.method == 'POST':
+        bot = ultraChatBot(request.json)
+        return bot.Processing_incoming_messages()
+    
+
+if (__name__) == '__main__':
+    app.run()
